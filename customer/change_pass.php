0336728066036fb5d0154fbcf52fb7e46b9726d9
Elias <eliashgiorgis@gmail.com>
2019-06-09 23:28:03 +0200

    Passwords are hashed when changed

--- customer/change_pass.php
+++ customer/change_pass.php
@@ -42,23 +42,26 @@
 
 <?php 
 
-if(isset($_POST['submit'])){
+if(isset($_POST['submit'])){ 
     
     $c_email = $_SESSION['customer_email'];
     
-    $c_old_pass = $_POST['old_pass'];
+    $c_old_pass = mysqli_real_escape_string($con, $_POST['old_pass']);
     
-    $c_new_pass = $_POST['new_pass'];
+    $c_new_pass = mysqli_real_escape_string($con, $_POST['new_pass']);
     
-    $c_new_pass_again = $_POST['new_pass_again'];
+    $c_new_pass_again = mysqli_real_escape_string($con, $_POST['new_pass_again']);
     
-    $sel_c_old_pass = "select * from tbl_customer where customer_pass='$c_old_pass'";
+    $sel_c_old_pass = "select * from tbl_customer where customer_email='$c_email'";
     
     $run_c_old_pass = mysqli_query($con,$sel_c_old_pass);
     
     $check_c_old_pass = mysqli_fetch_array($run_c_old_pass);
+
+    //De-hashing the password
+    $hashedpwdcheck = password_verify($c_old_pass, $check_c_old_pass['customer_pass']);
     
-    if($check_c_old_pass==0){
+    if ($hashedpwdcheck == false) {
         
         echo "<script>alert('Sorry, your current password is not valid. Please try again')</script>";
         
@@ -74,13 +77,16 @@ if(isset($_POST['submit'])){
         
     }
     
-    $update_c_pass = "update tbl_customer set customer_pass='$c_new_pass' where customer_email='$c_email'";
+    
+    $hashedpwd = password_hash($c_new_pass, PASSWORD_DEFAULT);
+
+    $update_c_pass = "update tbl_customer set customer_pass='$hashedpwd' where customer_email='$c_email'";
     
     $run_c_pass = mysqli_query($con,$update_c_pass);
     
     if($run_c_pass){
         
-        echo "<script>alert('Your password has been changed')</script>";
+        echo "<script>alert('Your password has been changed successfully')</script>";
         
         echo "<script>window.open('my_account.php?my_orders','_self')</script>";
         
